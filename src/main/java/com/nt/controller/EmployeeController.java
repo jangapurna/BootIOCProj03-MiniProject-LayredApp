139f5493a06569c6cc91f48525100fbcf379e3db
Jpurna <jpurna86@gmail.com>
2025-04-27 16:23:57 -0400

    Commit

--- src/main/java/com/nt/controller/EmployeeController.java
+++ src/main/java/com/nt/controller/EmployeeController.java
@@ -17,6 +17,7 @@ public class EmployeeController {
 	
 	public List<Employee> getEmployeesBydesign(String desg1,String desg2, String desg3) throws Exception
 	{
+		System.out.println("This is EmployeeController");
 		List<Employee> emplist = employeeService.searchEmployessByDesg(desg1, desg2, desg3);
 		return emplist;
 		
